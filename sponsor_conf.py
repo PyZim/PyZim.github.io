32374ee256641e354cbc8d4857a2fb7e1066a389
JustNunuz <nunudzaimrewa222@gmail.com>
2024-10-24 17:05:28 +0200

    Sponsor demarcation

--- sponsor_conf.py
+++ sponsor_conf.py
@@ -2,11 +2,14 @@ PLATINUM_SPONSORS = [
     ("psf-logo.png", "https://www.python.org/psf-landing/"),
 ]
 
+BRONZE_SPONSOR=[
+    "bpd-logo.png", "https://blackpythondevs.com"
+]
+
 COMMUNITY_SPONSORS = [
     ("django-logo.png", "https://www.djangoproject.com/foundation/"),
     ("linux-foundation.png", "https://www.linuxfoundation.org/"),
     ("defna-logo.png", "https://www.defna.org/about/"),
-    ("aws-logo.png", "https://aws.amazon.com"),
-    ("bpd-logo.png", "https://blackpythondevs.com"),
+    ("aws-logo.png", "https://aws.amazon.com")
 ]
 
